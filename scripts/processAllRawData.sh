62dac967da6f78e2301bf767250221c44f811f18
amcp <amcp@mit.edu>
2012-12-27 16:22:21 -0500

    fixed processAll script

--- scripts/processAllRawData.sh
+++ scripts/processAllRawData.sh
@@ -19,15 +19,61 @@
 # You should have received a copy of the GNU General Public License
 # along with Worth. If not, see http://www.gnu.org/licenses/.
 
-rm -rf $1/data
+if [ ! -d "$1/data" ]; then
+    mkdir -p $1/data
+fi
 
-$1/scripts/processCaEstimatedDeductions.sh $1/raw/$2_CA_ESTIMATED_DEDUCTION.txt $1/data/$2_CA_ESTIMATED_DEDUCTION.txt.dat
-$1/scripts/processCaExemptionAllowances.sh $1/raw/$2_CA_EXEMPTION_ALLOWANCE.txt $1/data/$2_CA_EXEMPTION_ALLOWANCE.txt.dat
-$1/scripts/processCaFilingStatusFrequencyTable.sh $1/raw/$2_CA_LOW_INCOME.txt $1/data/$2_CA_LOW_INCOME.txt.dat
-$1/scripts/processCaFilingStatusFrequencyTable.sh $1/raw/$2_CA_STANDARD_DEDUCTION.txt $1/data/$2_CA_STANDARD_DEDUCTION.txt.dat
-$1/scripts/processCaWithholdingTable.sh $1/raw/$2_CA_HEADOFHOUSEHOLD.txt $1/data/$2_CA_HEADOFHOUSEHOLD.txt.dat
-$1/scripts/processCaWithholdingTable.sh $1/raw/$2_CA_SINGLE.txt $1/data/$2_CA_SINGLE.txt.dat
-$1/scripts/processCaWithholdingTable.sh $1/raw/$2_CA_MARRIED.txt $1/data/$2_CA_MARRIED.txt.dat
+if [ -e "$1/raw/$2_CA_ESTIMATED_DEDUCTION.txt" ]; then
+    $1/scripts/processCaEstimatedDeductions.sh $1/raw/$2_CA_ESTIMATED_DEDUCTION.txt $1/data/$2_CA_ESTIMATED_DEDUCTION.txt.dat
+else
+    echo "$1/raw/$2_CA_ESTIMATED_DEDUCTION.txt does not exist."
+fi
 
-$1/scripts/processUsWithholdingTables.sh $1/raw/$2_US_SINGLE_MARRIED.txt $1/data/$2_US
-$1/scripts/processUsWithholdingAllowances.sh $1/raw/$2_US_WITHHOLDING_ALLOWANCE.txt $1/data/$2_US_WITHHOLDING_ALLOWANCE.txt.dat
+if [ -e "$1/raw/$2_CA_EXEMPTION_ALLOWANCE.txt" ]; then
+    $1/scripts/processCaExemptionAllowances.sh $1/raw/$2_CA_EXEMPTION_ALLOWANCE.txt $1/data/$2_CA_EXEMPTION_ALLOWANCE.txt.dat
+else
+    echo "$1/raw/$2_CA_EXEMPTION_ALLOWANCE.txt does not exist."
+fi
+
+if [ -e "$1/raw/$2_CA_LOW_INCOME.txt" ]; then
+    $1/scripts/processCaFilingStatusFrequencyTable.sh $1/raw/$2_CA_LOW_INCOME.txt $1/data/$2_CA_LOW_INCOME.txt.dat
+else
+    echo "$1/raw/$2_CA_LOW_INCOME.txt does not exist."
+fi
+
+if [ -e "$1/raw/$2_CA_STANDARD_DEDUCTION.txt" ]; then
+    $1/scripts/processCaFilingStatusFrequencyTable.sh $1/raw/$2_CA_STANDARD_DEDUCTION.txt $1/data/$2_CA_STANDARD_DEDUCTION.txt.dat
+else
+    echo "$1/raw/$2_CA_STANDARD_DEDUCTION.txt does not exist."
+fi
+
+if [ -e "$1/raw/$2_CA_HEADOFHOUSEHOLD.txt" ]; then
+    $1/scripts/processCaWithholdingTable.sh $1/raw/$2_CA_HEADOFHOUSEHOLD.txt $1/data/$2_CA_HEADOFHOUSEHOLD.txt.dat
+else
+    echo "$1/raw/$2_CA_HEADOFHOUSEHOLD.txt does not exist."
+fi
+
+if [ -e "$1/raw/$2_CA_SINGLE.txt" ]; then
+    $1/scripts/processCaWithholdingTable.sh $1/raw/$2_CA_SINGLE.txt $1/data/$2_CA_SINGLE.txt.dat
+else
+    echo "$1/raw/$2_CA_SINGLE.txt does not exist."
+fi
+
+if [ -e "$1/raw/$2_CA_MARRIED.txt" ]; then
+    $1/scripts/processCaWithholdingTable.sh $1/raw/$2_CA_MARRIED.txt $1/data/$2_CA_MARRIED.txt.dat
+else
+    echo "$1/raw/$2_CA_MARRIED.txt does not exist."
+fi
+
+if [ -e "$1/raw/$2_US_SINGLE_MARRIED.txt" ]; then
+    $1/scripts/processUsWithholdingTables.sh $1/raw/$2_US_SINGLE_MARRIED.txt $1/data/$2_US
+else
+    echo "$1/raw/$2_US_SINGLE_MARRIED.txt does not exist."
+fi
+
+if [ -e "$1/raw/$2_US_WITHHOLDING_ALLOWANCE.txt" ]; then
+    $1/scripts/processUsWithholdingAllowances.sh $1/raw/$2_US_WITHHOLDING_ALLOWANCE.txt $1/data/$2_US_WITHHOLDING_ALLOWANCE.txt.dat
+else
+    echo "$1/raw/$2_US_WITHHOLDING_ALLOWANCE.txt does not exist."
+fi
+
